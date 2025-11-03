2414e9849de9e1bba0b460d5592e0967183c233e
Mehmet <salgarm@gmx.de>
2025-11-03 21:18:40 +0100

    Additional elements for fork

--- fork.sh
+++ fork.sh
@@ -134,4 +134,7 @@ sedCommand="${sedCommand}s|4eyes-statemachine|$1|g"
 sed "${sedCommand}" settings.gradle > settings_fork.gradle
 
 #gradle.properties
-sed "s|FourEyes|${camelCase}|g" gradle.properties > gradle_fork.properties
+sedCommand="s|FourEyes|${camelCase}|g"
+sedCommand="${sedCommand}s|org.salgar.fsm.pekko.foureyes|$2|g"
+sedCommand="${sedCommand}s|4eyes|$1|g"
+sed "${sedCommand}" gradle.properties > gradle_fork.properties
